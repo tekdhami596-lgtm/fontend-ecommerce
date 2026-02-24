529794363ddf02e1cbb2dcb1410216df530f5d79
tekdhami596--lgtm <tekdhami596@gmail.com>
2026-02-24 06:42:22 +0545

    cookie

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  base: "/",
 });
