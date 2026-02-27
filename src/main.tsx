48236e105757c509931a722844b15100c0bf8bcc
tekdhami596--lgtm <tekdhami596@gmail.com>
2026-02-27 14:50:11 +0545

    updated loading

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,10 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-setTimeout(() => {
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>,
-  );
-}, 2000); // 2 second delay just for testing
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+);
